9034cbdeabd4c545e330d9ee85f930fdf50bf393
hhz1527411634 <38418334+hhz1527411634@users.noreply.github.com>
2024-02-22 14:43:04 +0800

    Update how-to-trigger.vue

--- docs/examples/dropdown/how-to-trigger.vue
+++ docs/examples/dropdown/how-to-trigger.vue
@@ -8,11 +8,11 @@
         </span>
         <template #dropdown>
           <el-dropdown-menu>
-            <el-dropdown-item :icon="Plus">Action 1</el-dropdown-item>
+            <el-dropdown-item :icon="Plus">Action 1111</el-dropdown-item>
             <el-dropdown-item :icon="CirclePlusFilled">
               Action 2
             </el-dropdown-item>
-            <el-dropdown-item :icon="CirclePlus">Action 3</el-dropdown-item>
+            <el-dropdown-item :icon="CirclePlus">Action 1113</el-dropdown-item>
             <el-dropdown-item :icon="Check">Action 4</el-dropdown-item>
             <el-dropdown-item :icon="CircleCheck">Action 5</el-dropdown-item>
           </el-dropdown-menu>
